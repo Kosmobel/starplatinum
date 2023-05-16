2bedd07f34e1c408e128938dcaf58cda3fb686ce
Kosmobel <131285977+Kosmobel@users.noreply.github.com>
2023-05-16 21:56:08 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.nav');
+
+menu.style.display = 'none';
+
+menuBtn.addEventListener('click', function() {
+  if (menu.style.display === 'none') {
+    menu.style.display = 'grid';
+    menu.style.position = 'fixed';
+    menuBtn.style.background = "green"
+  } else {
+    menu.style.display = 'none';
+    menuBtn.style.background = "#333"
+  }
+});
